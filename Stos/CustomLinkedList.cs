87ac63664d42422384460db9303ceae4069c232d
wiechecdawid <wiechec.dawid@gmail.com>
2020-12-23 02:50:10 +0100

    Task 7 started

---
+++ Stos/CustomLinkedList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stos
+{
+    public class CustomLinkedList<T>
+    {
+        private class Node
+        {
+            private T _node;
+            
+            public Node(T node)
+            {
+                _node = node;
+            }
+
+            public T Next { get; set; }
+        }
+    }
+}
