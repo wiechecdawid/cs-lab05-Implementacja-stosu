23f87129b5c4a3e5715e2bd3c891a3fbaace2b4d
wiechecdawid <wiechec.dawid@gmail.com>
2020-12-25 23:15:20 +0100

    Task 7 Start of linked list implementation

--- Stos/CustomLinkedList.cs
+++ Stos/CustomLinkedList.cs
@@ -4,18 +4,88 @@ using System.Text;
 
 namespace Stos
 {
-    public class CustomLinkedList<T>
+    public class CustomLinkedList<T> : ICollection<T>, IEnumerable<T> 
     {
+        private Node[] _nodes;
+        private int _current = -1;
+
+        public CustomLinkedList()
+        {
+            _nodes = new Node[10];
+        }
+
+        int Count => _current + 1;
+
+        public void Add(T node)
+        {
+            if(_current == _nodes.Length - 1)
+                Array.Resize(ref _nodes, _nodes.Length * 2)
+
+            var n = new Node(node);
+
+            _current++;
+            _nodes[current] = n;
+
+            if(current > 0)
+                _nodes[current -1].Next = n;
+        }
+
+        public void Clear()
+        {
+            _nodes = new Node[10];
+        }
+
+        public bool Contains(T item)
+        {
+            if(item == null)
+            {
+                foreach(var n in _nodes)
+                {
+                    if(n == null)
+                        return true;
+                }
+            }
+            else
+            {     
+                var c = new EqualityComparer<T>.Default;           
+                foreach(var n in _nodes)
+                {
+                    if(c.Equals(n.Head, item))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+
+        }
+
+        public bool Remove(T item)
+        {
+            for(i = 0; i < _nodes.Length; i++)
+            {
+
+            }
+        }
+
+        public Collections.IEnumerable GetEnumerator() => GetEnumerator();
+
+        public IEnumerable<T> GetEnumerator()
+        {
+            for(int i = 0; i < _current; i++)
+                yield return (_nodes[i]);
+        }
+
         private class Node
         {
-            private T _node;
-            
             public Node(T node)
             {
-                _node = node;
+                Head = node;
             }
-
-            public T Next { get; set; }
+            public T Head {get; set; }
+            public T Next { get; set; } = null;
         }
     }
 }
