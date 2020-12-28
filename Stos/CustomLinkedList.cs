0cf13cb9b43f05de9e71000631e843b120925599
wiechecdawid <wiechec.dawid@gmail.com>
2020-12-28 13:26:39 +0100

    Custom linked list implementded

--- Stos/CustomLinkedList.cs
+++ Stos/CustomLinkedList.cs
@@ -1,91 +1,168 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Stos
 {
+    //Simple linked list
     public class CustomLinkedList<T> : ICollection<T>, IEnumerable<T> 
     {
-        private Node[] _nodes;
-        private int _current = -1;
 
-        public CustomLinkedList()
+        private Node<T> _head;
+        private int _current = -1;
+        bool _isReadOnly = false;
+
+        public Node<T> First { get => _head; }
+        public Node<T> Last
         {
-            _nodes = new Node[10];
+            get => _current > 0 ? this[_current] : _head;
         }
 
         int Count => _current + 1;
 
-        public void Add(T node)
+        int ICollection<T>.Count => Count;
+
+        public bool IsReadOnly => _isReadOnly;
+
+
+        protected Node<T> this[int i]
         {
-            if(_current == _nodes.Length - 1)
-                Array.Resize(ref _nodes, _nodes.Length * 2)
+            get
+            {
+                if (i > _current)
+                    throw new IndexOutOfRangeException();
+                Node<T> node = _head;
 
-            var n = new Node(node);
+                for (int j = 1; j <= i; j++)
+                    node = node.Next;
 
-            _current++;
-            _nodes[current] = n;
-
-            if(current > 0)
-                _nodes[current -1].Next = n;
+                return node;
+            }
         }
 
+
+        public void Add(T node)
+        {
+            if(!IsReadOnly && node != null)
+            {
+                Node<T> newNode = new Node<T>
+                {
+                    Head = node,
+                    Next = null
+                };
+
+                if (_current == -1)
+                {
+                    _head = newNode;
+                }
+                else
+                {
+                    Node<T> tail = _head;
+
+                    while (tail.Next != null)
+                        tail = tail.Next;
+
+                    tail.Next = newNode;
+                }
+                _current++;
+            }
+        }
+        
         public void Clear()
         {
-            _nodes = new Node[10];
+            _head = null;
         }
 
         public bool Contains(T item)
         {
-            if(item == null)
+            if(item == null && _head == null)
             {
-                foreach(var n in _nodes)
-                {
-                    if(n == null)
-                        return true;
-                }
+                return true;
             }
-            else
-            {     
-                var c = new EqualityComparer<T>.Default;           
-                foreach(var n in _nodes)
-                {
-                    if(c.Equals(n.Head, item))
-                        return true;
-                }
+
+            EqualityComparer<T> ct = EqualityComparer<T>.Default;
+
+            var node = _head;
+            do
+            {
+                if (ct.Equals(node.Head, item))
+                    return true;
             }
+            while ((node = node.Next) != null);
+
             return false;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
+            if (array == null)
+            {
+                throw new ArgumentNullException("array");
+            }
 
+            if (arrayIndex < 0 || arrayIndex > array.Length)
+            {
+                throw new ArgumentOutOfRangeException("arrayIndex");
+            }
+
+            var node = _head;
+            if(node != null)
+            {
+                do
+                {
+                    array[arrayIndex++] = node.Head;
+                    node = node.Next;
+                }
+                while (node != null);
+            }
         }
 
         public bool Remove(T item)
         {
-            for(i = 0; i < _nodes.Length; i++)
-            {
+            var c = EqualityComparer<T>.Default;
+            var node = _head;
 
+            if (c.Equals(item, node.Head))
+            {
+                _head = this[1];
+                _current--;
+
+                return true;
+            }
+
+            while(node.Next != null)
+            {
+                if(c.Equals(node.Next.Head, item))
+                {
+                    node.Next = node.Next.Next;
+                    _current--;
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = _head;
+
+            for (int i = 0; i <= _current; i++)
+            {
+                if (i > 0) node = node.Next;
+
+                yield return node.Head;
             }
         }
 
-        public Collections.IEnumerable GetEnumerator() => GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
 
-        public IEnumerable<T> GetEnumerator()
-        {
-            for(int i = 0; i < _current; i++)
-                yield return (_nodes[i]);
-        }
-
-        private class Node
-        {
-            public Node(T node)
-            {
-                Head = node;
-            }
-            public T Head {get; set; }
-            public T Next { get; set; } = null;
-        }
+    public class Node<T>
+    {
+        public T Head { get; set; }
+        public Node<T> Next { get; set; }
     }
 }
